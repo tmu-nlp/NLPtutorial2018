f6814eb092d40aa02d5271d0fa6aa4236e4aa857
asdaspl <feelmailczs@gmail.com>
2018-04-16 16:44:55 +0900

    Update 00-homework.py

--- zchen/tutorial00/00-homework.py
+++ zchen/tutorial00/00-homework.py
@@ -5,8 +5,8 @@ count_dict = defaultdict(lambda : 0)
 
 with open(sys.argv[1], 'r') as fr:
     for line in fr:
-        for token in line.split(" "):
-            count_dict[token.strip()] += 1
+        for token in line.strip().split(" "):
+            count_dict[token] += 1
 
 for pair in count_dict.items():
     print( *pair )
