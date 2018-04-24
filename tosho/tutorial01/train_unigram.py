66a270c9b9bfa7251aae430351718af266322ae1
Tosho Hirasawa <tosho.hirasawa@gmail.com>
2018-04-24 17:33:30 +0900

    tested UniGram with test data

--- tosho/tutorial01/train_unigram.py
+++ tosho/tutorial01/train_unigram.py
@@ -1,8 +1,9 @@
 import os, sys
 sys.path.append(os.path.pardir)
 from tutorial00.word_count import count_words, load_file
-import math
 from random import sample
+import math
+import pickle
 
 class UniGram:
     def __init__(self, verbose=False):
@@ -21,21 +22,29 @@ class UniGram:
         # 単語ごとの確率を計算する
         nk_rate = 1. - unk_rate
         N = sum(word_dict.values())
-        self.words = word_dict
+        self.words = dict(word_dict)
         for key, value in self.words.items():
             self.words[key] = nk_rate * value / N + self.unk
 
         if self.verbose:
-            print(f'Unknow Rate : {self.unk}')
-            print(f'Trained words (sample) :')
-            for word, rate in sample(self.words.items(), min(10, len(self.words))):
-                print(f'{word} : {rate}')
+            self.__print_params()
+
+    def __print_params(self):
+        print(f'Unknow Rate : {self.unk}')
+        print(f'Trained words (sample) :')
+        for word, rate in sample(self.words.items(), min(10, len(self.words))):
+            print(f'{word} : {rate}')
 
     def load(self, filename):
-        pass
+        with open(filename, 'rb') as fp:
+            self.words, self.unk = pickle.load(fp)
+
+        if self.verbose:
+            self.__print_params()
 
     def save(self, filename):
-        pass
+        with open(filename, 'wb') as fp:
+            pickle.dump((self.words, self.unk), fp)
 
     def estimate(self, seq):
         entropy = 0.
@@ -64,30 +73,26 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('path_to_train_file')
-    parser.add_argument('path_to_test_file')
+    parser.add_argument('path_to_cache_file')
     parser.add_argument('--debug', action='store_true', default=False)
     parser.add_argument('--includes-eos', action='store_true', default=False)
+    parser.add_argument('--verify-params', action='store_true', default=False)
     arg = parser.parse_args()
 
     print('training model...')
 
     train_file = arg.path_to_train_file
-    test_file = arg.path_to_test_file
 
     words = count_words(train_file, includes_eos=arg.includes_eos)
     model = UniGram(arg.debug)
     model.train(words)
 
-    print('testing model...')
+    model.save(arg.path_to_cache_file)
 
-    test_data = open(test_file, 'r')
-    lines = load_file(test_data, arg.includes_eos)
+    if arg.verify_params:
+        model2 = UniGram(arg.debug)
+        model2.load(arg.path_to_cache_file)
 
-    model.estimate(lines)
-
-    print(f'entropy = {model.entropy}')
-    print(f'converage = {model.coverage}')
-    print(f'perplexity = {model.perplexity}')
-
-    test_data.close()    
-    
+        print(f'UNK | actual : {model.unk}, saved : {model2.unk}')
+        for key in sample(model.words.keys(), min(len(model.words), 10)):
+            print(f'{key} | actual : {model.words[key]}, saved : {model2.words[key]}')
