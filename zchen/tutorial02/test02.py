1c99a71330eb70787ecc62fc4ef4dbad7e2240df
asdaspl <feelmailczs@gmail.com>
2018-05-11 19:30:30 +0900

    Add doc specification to some functions

--- zchen/tutorial02/test02.py
+++ zchen/tutorial02/test02.py
@@ -10,13 +10,33 @@ a     0.250000
 b     0.250000
 </s>  0.250000
 ----
-2-gram model based on 8 tokens in 6 types
+2-gram model based on 8 tokens in 6 types with Witten-Bell weights
 <s>, a   0.250000
 b, c     0.500000
 b, d     0.500000
 a, b     1.000000
 c, </s>  1.000000
 d, </s>  1.000000
+	- Witten Bell weights:
+	w'<s>'          0.666667
+	w'a'            0.666667
+	w'b'            0.500000
+	w'c'            0.500000
+	w'd'            0.500000
+'''
+answer_3 = answer + \
+'''----
+3-gram model based on 8 tokens in 6 types with Witten-Bell weights
+<s>, <s>, a  0.250000
+a, b, c      0.500000
+a, b, d      0.500000
+<s>, a, b    1.000000
+b, c, </s>   1.000000
+b, d, </s>   1.000000
+	- Witten Bell weights:
+	w'<s>'          0.666667
+	w'a'            0.500000
+	w'b'            0.500000
 '''
 
 class TestNGramMethods(unittest.TestCase):
@@ -40,6 +60,16 @@ class TestNGramMethods(unittest.TestCase):
         model = N_Gram_Family(2, "dummy")
         model.load()
         self.assertEqual(model.entropy_of("../../test/02-train-input.txt", None, 1000000), -0.9790540358424883)
+
+    def test_train_3(self):
+        model = N_Gram_Family(3, "dummy")
+        model.seal_model("../../test/02-train-input.txt")
+        self.assertEqual(str(model), answer_3)
+
+    def test_load_3(self):
+        model = N_Gram_Family(3, "dummy")
+        model.load()
+        self.assertEqual(str(model), answer_3)
 
 if __name__ == "__main__":
     unittest.main()
--- zchen/utils.py
+++ zchen/utils.py
@@ -20,6 +20,7 @@ def make_padding(n):
 
 
 def count_n_gram_from_file(n, fr):
+    '''Turn file into n-gram count with affixes'''
     count_dict = Counter()
     # make padding
     sos, eos = make_padding( n-1 )
@@ -33,6 +34,7 @@ def count_n_gram_from_file(n, fr):
 
 
 def cond_prob(count, prefix_count):
+    '''Turn counting into probability.'''
     total = sum(count.values())
     sos = "<%s>" % s_tok
     if prefix_count:
@@ -46,6 +48,7 @@ def cond_prob(count, prefix_count):
 
 
 def witten_bell_weights(ngram_count):
+    '''A special use case of \'defaultdict\''''
     type_variation_count = defaultdict(lambda:[0,set()])
     for ngram, count in ngram_count.items():
         t = type_variation_count[ngram[0]]
@@ -55,10 +58,12 @@ def witten_bell_weights(ngram_count):
 
 
 def unigram_smooth_gen(mod_prob, total_num_types):
+    '''Special funtion for unigram.'''
     return lambda p: interpolate_gen(mod_prob)(p, 1 / total_num_types)
 
 
 def interpolate_gen(main_prob):
+    '''Produce fixed functions for interpolation, for the sake of computation'''
     if 0 == main_prob:
         return lambda p1, p2: p2
     elif 1 == main_prob:
@@ -70,6 +75,7 @@ def interpolate_gen(main_prob):
 
 
 def _check_oov_call(oov_func):
+    '''Validator for counting oov at test.'''
     def wrapper(*args):
         if args[0]._N_Gram__iv_counter is None:
             msg = "Calling '%s' before 'prof_of'" % oov_func.__name__
@@ -91,6 +97,8 @@ class N_Gram:
             return count_n_gram_from_file(self._n_gram, fr)
 
     def seal_model(self, count_dict, prefix_count_dict = None):
+        '''When adding more parameters, save them in the tuple for sealing them.
+        Save them in the form (elem1, elem2,) + another_tuple. Don\'t forget to delete.'''
         witten_bell = witten_bell_weights(count_dict) if self._n_gram > 1 else None
         self._model = (self._n_gram, witten_bell) + cond_prob(count_dict, prefix_count_dict)
         with open(self._model_file, "wb") as fw:
@@ -122,20 +130,27 @@ class N_Gram:
 
     def __str__(self):
         model = self._model[-1]
-        s = "%d-gram model based on %d tokens in %s types\n"
+        s = "%d-gram model based on %d tokens in %s types"
+        s += " with Witten-Bell weights\n" if self.num_gram > 1 else '\n'
         s = s % (self.num_gram, self.num_tokens, self.num_types)
         ml = max(sum(len(ngi)+2 for ngi in ng) for ng in model.keys())
         lop = "%-{}s%f\n".format(ml)
         for k, v in sorted(model.items(), key = lambda x:x[1]):
             s += lop % (", ".join(k), v)
+        if self.num_gram > 1:
+            s += "\t- Witten Bell weights:\n"
+            for k,w in self._model[1].items():
+                s += "\tw%-15s" % ("'" + k + "'") + '%f\n' % w
         return s
 
-    def prob_of(self, test_file, count = False):
+    def prob_of(self, test_file, count_oov = False):
+        '''Should test the file with conditional chain.
+        Multiply the yielded value or add log-linearly.'''
         # num_tokens is useless
-        n, wb, _, model = self._model
+        n, _, _, model = self._model
         sos, eos = make_padding(n - 1)
-        self.__iv_counter = Counter() if count else None
-        self._oov_counter = Counter() if count else None
+        self.__iv_counter = Counter() if count_oov else None
+        self._oov_counter = Counter() if count_oov else None
 
         with open(test_file, 'r') as fr:
             for line in fr:
@@ -143,10 +158,10 @@ class N_Gram:
                 for ng in n_gram(n, lot):
                     tok = ng[-1]
                     if ng in model:
-                        if count: self.__iv_counter[ng] += 1
+                        if count_oov: self.__iv_counter[ng] += 1
                         yield tok, model[ng]
                     else:
-                        if count: self._oov_counter[ng] += 1
+                        if count_oov: self._oov_counter[ng] += 1
                         yield tok, 0.0
 
     @property
@@ -180,6 +195,8 @@ class N_Gram_Family:
         return "----\n".join(str(s) for s in self._family)
 
     def seal_model(self, train_file):
+        '''The condition probability of N-Gram model stand on (N-1)-Gram model.
+        Seal the models from 1-gram to n-gram'''
         pre_count = None
         for model in self._family:
             count = model.build_model(train_file)
@@ -191,6 +208,9 @@ class N_Gram_Family:
             model.load()
 
     def entropy_of(self, test_file, weights, num_types_include_oov, base = None):
+        '''About weights:
+            When weights is None, apply Witten-Bell smooth to each word;
+            When weights is a list of fixed numbers for each model, apply them to each model.'''
         assert weights is None or len( weights ) == len( self._family )
         if weights:
             assert all( 0 <= w <= 1 for w in weights)
@@ -203,7 +223,7 @@ class N_Gram_Family:
         log_prob   = 0
         processing = True
         while processing: # for a token in each n-gram model
-            w_prob_by_last_model = 1 / num_types_include_oov
+            w_prob_by_last_model = 1 / num_types_include_oov # Zero-Gram
             for model, model_gen, weight in zip(self._family, family, weights):
                 try:
                     w, w_prob = next(model_gen)
