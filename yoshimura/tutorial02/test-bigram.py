316ebdee29ea2dac4c13489e47e91d339449ecf6
YoshimuraRyoma <Ryoma@YoshimuraRyoma-no-MacBook-Air.local>
2018-05-14 17:39:15 +0900

    fixed

--- yoshimura/tutorial02/test-bigram.py
+++ yoshimura/tutorial02/test-bigram.py
@@ -2,6 +2,10 @@ import math
 from collections import defaultdict
 
 V = 10**6
+lam1 = 0.95
+lam2 = 0.95
+H = 0
+W = 0
 
 path_test = '../../data/wiki-en-test.word'
 
@@ -15,36 +19,49 @@ with open('model','r',encoding = 'utf-8') as model_file:
         probs[model[0]] = float(model[1])
         witten_lambda[model[0]] = float(model[2])
 
-# 評価
-with open(path_test,'r',encoding = 'utf-8') as test_file:
-    for line in test_file:
-        line = line.strip()
-        words = line.split(' ')
-        words.insert(0,'<s>')
-        words.append('</s>')
-
-    entropy = {}
+# グリッドサーチ
+entropy = {}
+for lam1 in [0.01 * n for n in range(1, 100)]:
     for lam2 in [0.01 * n for n in range(1, 100)]:
-        for lam1 in [0.01 * n for n in range(1, 100)]:
-            W = 0
-            H = 0
-            for i in range(1, len(words)):
-                bigram = words[i-1] + ' ' + words[i]
-                P1 = lam1 * probs[words[i]] + (1-lam1)/V 
-                # P1 = witten_lambda[words[i]] * probs[words[i]] + (1 - witten_lambda[words[i]])/V 
-                P2 = lam2 * probs[bigram] + (1-lam2) * P1 
-                # P2 = witten_lambda[bigram] * probs[bigram] + (1 - witten_lambda[bigram]) * P1 
-                H += -math.log2(P2)
-                W += 1
-            entropy[H/W] = [lam1, lam2]
-    
-    # with open('grid','w', encoding = 'utf-8') as file_grid:
-    #     for key in sorted(entropy.keys()):
-    #         file_grid.write(f'{key}\t{entropy[key][0]}\t{entropy[key][1]}\n') 
-    
-    print(f'entropy = {min(entropy.keys())}')
+        with open(path_test,'r',encoding = 'utf-8') as test_file:
+            for line in test_file:
+                line = line.strip()
+                words = line.split(' ')
+                words.insert(0,'<s>')
+                words.append('</s>')
+                    
+                for i in range(1, len(words)):
+                    bigram = words[i-1] + ' ' + words[i]
+                    P1 = lam1 * probs[words[i]] + (1-lam1)/V 
+                    P2 = lam2 * probs[bigram] + (1-lam2) * P1 
+                    H += -math.log2(P2)
+                    W += 1
 
-# python test-bigram.py ../../data/wiki-en-train.word
+            entropy[(lam1,lam2)] = H/W
 
+min_en = min(entropy.values())
+opt_lam = [key for key, value in entropy.items() if value == min_en][0]
+
+print(f'optimaized entropy = {min(entropy.values())}')
+print(f'lam1 = {opt_lam[0]}')
+print(f'lam1 = {opt_lam[1]}')
+    
+# witten-bell
+# with open(path_test,'r',encoding = 'utf-8') as test_file:
+#     for line in test_file:
+#         line = line.strip()
+#         words = line.split(' ')
+#         words.insert(0,'<s>')
+#         words.append('</s>')
+            
+#         for i in range(1, len(words)):
+#             bigram = words[i-1] + ' ' + words[i]
+#             P1 = witten_lambda[words[i]] * probs[words[i]] + (1 - witten_lambda[words[i]])/V 
+#             P2 = witten_lambda[bigram] * probs[bigram] + (1 - witten_lambda[bigram]) * P1 
+#             H += -math.log2(P2)
+#             W += 1
+#     print(f'entropy = {H/W}')
+
+        
 # 11.2848 0.95 0.95
 
