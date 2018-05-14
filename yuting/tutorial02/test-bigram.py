6da7caa5bf967dce863a0e3e9abec8ac6456bf76
koala <koala@koala-no-MacBook-Air.local>
2018-05-14 14:44:14 +0900

    02

--- yuting/tutorial02/test-bigram.py
+++ yuting/tutorial02/test-bigram.py
@@ -20,8 +20,10 @@ with open(sys.argv[1],'r') as test_file:
 		words.insert(0,"<s>")
 		for i in range(1,len(words)):
 			P1=l1 * probs[words[i]] + (i-l1)/V
-			P2=l2 * probs[f'{words[i-1]}{words[i]}'] + (1-L2)*P1
+			P2=l2 * probs[f'{words[i-1]}{words[i]}'] + (1-l2)*P1
 			H += -math.log(P2)
 			W += 1
 
 print(f'entropy = {H/W}')
+
+#entropy = 14.49282696349011
