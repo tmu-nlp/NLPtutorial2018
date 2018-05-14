7478015837db89a11033680927aee672dacf023c
one <one@onenoMacBook-Air.local>
2018-05-14 15:08:32 +0900

    commit

--- ando/tutorial02/gakushu.py
+++ ando/tutorial02/gakushu.py
@@ -10,7 +10,7 @@ for line in lines:
     line = line.rstrip()
     words = line.split(" ")
     words.append("</s>")
-    words.insert(0, “<s>”) 
+    words.insert(0,"<s>") 
     for word in words:
         if word in dic:
             dic[word] += 1
@@ -18,7 +18,7 @@ for line in lines:
             dic[word] = 1
             total += 1
         token+=1
-        if word != “<s>”:
+        if word != "<s>":
             if lastword not in dic2gram:
                 dic2gram[lastword] = {}
             else:
@@ -31,10 +31,10 @@ for line in lines:
 
 with open("model.txt", "w") as fout:
     for i,j in dic.items():
-        fout.write("{}:{}\t".format(i,j/token))
+        fout.write("{} {}\t".format(i,j/token))
 with open("model2.txt", "w") as fout:
     for i in dic2gram:
         for j in dic2gram[i]:
             num = dic2gram[i][j]
             dic2gram[i][j] = num/dic[i]        
-            fout.write(“{}:{}:{}\t”.format(i,j,dic2gram[i][j]))
+            fout.write("{} {} {}\t".format(i,j,dic2gram[i][j]))
--- ando/tutorial02/yomikomi.py
+++ ando/tutorial02/yomikomi.py
@@ -9,13 +9,13 @@ f = open("model.txt").read()
 f = f.rstrip()
 words = f.split("\t")
 for word in words:
-    word = word.split(":")
+    word = word.split(" ")
     dic[word[0]] = word[1]
 f = open("model2.txt").read()
 f = f.rstrip()
 words = f.split("\t")
 for word in words:
-    word = word.split(":")
+    word = word.split(" ")
     if word[0] not in dic2gram:
         dic2gram[word[0]] = {}
         dic2gram[word[0]][word[1]]=float(word[2])
@@ -28,10 +28,10 @@ for line in lines:
     line = line.rstrip()
     test_words = line.split(" ")
     test_words.append("</s>")
-    test_words.insert(0, “<s>”) 
+    test_words.insert(0,"<s>") 
     wordcount += len(test_words)
     for i in test_words:
-        if i != “<s>”:
+        if i != "<s>":
             if i in dic:
                 one = 0.95*(float(dic[i]))+0.05*(1/1000000)
             else:
@@ -41,5 +41,6 @@ for line in lines:
                     partcul *= 0.95*(float(dic2gram[i][lastword]))+0.05*one
                 else:
                     partcul *= 0.05*one
+        lastword = i
     cul += math.log(partcul,2)
 print("entropy="+str(-(cul/wordcount)))
