5a28620f4ace8ee361581f3a885f91d9fd6306d9
asdaspl <feelmailczs@gmail.com>
2018-05-28 19:22:08 +0900

    Update plot

--- zchen/tutorial04/tsv/viewer.R
+++ zchen/tutorial04/tsv/viewer.R
@@ -9,7 +9,7 @@ data = read.delim(args[1])
 p <- ggplot(data, aes(reorder(paste(id, stringr::str_wrap(seq, 10), sep = "\n"), id), postag, fill = cost)) +
     geom_bin2d( stat = "identity", show.legend = F) +
     #scale_fill_gradientn(colours = terrain.colors(10)) +
-    scale_fill_gradient(low = "black", high = "white") +
+    scale_fill_gradient(low = "white", high = "black") +
     labs( x = 'sentence', y = "POS tags", title = "Cost")
 
 ggsave(paste(args[1], ".png", sep = ""), width = nrow(data) / 70, height = 10)
