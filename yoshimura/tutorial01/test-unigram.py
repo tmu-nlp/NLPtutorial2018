192f18efaa67acbf240c0a1576f57d96bc699840
YoshimuraRyoma <Ryoma@YoshimuraRyoma-no-MacBook-Air.local>
2018-05-28 14:07:59 +0900

    fix code

--- yoshimura/tutorial01/test-unigram.py
+++ yoshimura/tutorial01/test-unigram.py
@@ -30,7 +30,7 @@ with open(sys.argv[2],"r") as test_file:
             else:
                 P = lam_unknown / V
                 unknown += 1 
-            print(f"P({word})\t = {lam} * {P_ml} + {lam_unknown:.2f} / {V} = {P}")
+            # print(f"P({word})\t = {lam} * {P_ml} + {lam_unknown:.2f} / {V} = {P}")
             H += -math.log(P,2)
 
 
@@ -46,32 +46,3 @@ print(f"coverage = {(W - unknown)/W}")
     
         
         
-    
-
-# test_file = open(sys.argv[2],"r")
-# for line in test_file:
-#     line = line.strip()
-#     words = line.split(" ")
-#     words.append("</s>")
-#     for word in words:
-#         W += 1 # test dataの単語数カウント
-#         # 尤度計算
-#         count = 0
-#         for m_word, probability in probabilities.items(): # 学習言語モデルに含まれるテストの単語の数をカウント
-#             if word in m_word:
-#                 count += 1
-#         if count == 0:
-#             unknown += 1 #未知語カウント
-        
-#         # P_ml = count / len(probabilities)
-#         if word in probabilities:
-#             P_ml = probabilities[word]
-#             print(f"P_ml({word}) = {P_ml}")
-#         else:
-#             P_ml = 0
-
-#         # 未知語対応
-#         P = lam * float(P_ml) + lam_unknown / V
-#         print(f"P({word}) = {P}")
-#         H += -math.log(P,2)
-# test_file.close()
--- yoshimura/tutorial03/word_split.py
+++ yoshimura/tutorial03/word_split.py
@@ -1,40 +1,49 @@
 import math
+from collections import defaultdict
 best_edge = {}
 best_score = {}
 
 path_model = '../tutorial01/model'
-path_test = '../../test/04-input.txt'
+path_test = '../../data/wiki-ja-test.txt'
+# path_model = '../tutorial01/model_test' # テスト用モデルファイルパス
+# path_test = '../../test/04-input.txt' # テスト用ファイルパス
+
+lam = 0.98
+V = 10**6
 
 # モデル読み込み
-probs = {}
+probs = defaultdict(lambda:0)
 with open(path_model,"r") as model_file:
     for line in model_file:
-        line = line.strip()
+        line = line.rstrip()
         model = line.split("\t")
         probs[model[0]] = float(model[1])
 
 with open(path_test,'r') as data_file:
     for line in data_file:
+        line = line.rstrip()
         # 前向きステップ
         best_edge[0] = None
         best_score[0] = 0
         for word_end in range(1, len(line) + 1): 
             best_score[word_end] = 10**10
-            for word_begin in range(word_end - 1):
+            for word_begin in range(0, word_end):
                 word = line[word_begin : word_end] # 部分文字列を取得
                 if word in probs or len(word) == 1:
-                    prob = probs[word]
-                    score = best_score[word_begin] - math.log(prob)
+                    prob = lam * probs[word] + (1-lam)/V
+                    score = best_score[word_begin] - math.log2(prob)
                     if score < best_score[word_end]:
                         best_score[word_end] = score
                         best_edge[word_end] = (word_begin, word_end)
+        
+        # print(best_score)
         # 後ろ向きステップ
-        # words = []
-        # next_edge = best_edge[len(best_edge) - 1]
-        # while next_edge != None:
-        #     word = line[next_edge[0] : next_edge[1]]
-        #     words.append(word)
-        #     next_edge = best_edge[next_edge[0]]
-        # print(''.join(words[::-1]))
+        words = []
+        next_edge = best_edge[len(best_edge) - 1]
+        while next_edge:
+            word = line[next_edge[0] : next_edge[1]]
+            words.append(word)
+            next_edge = best_edge[next_edge[0]]
+        print(' '.join(words[::-1]))
 
                 
