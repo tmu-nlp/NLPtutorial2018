f48a41d45bb31bee15552dfd8c8e8880dc3a2b8d
Tosho Hirasawa <tosho.hirasawa@gmail.com>
2018-06-13 23:17:01 +0900

    add layers

--- tosho/common/functions.py
+++ tosho/common/functions.py
@@ -1,4 +1,29 @@
 import math
+import numpy as np
+
+def softmax(x):
+    c = np.max(x)
+    exp_a = np.exp(x - c) # avoid overflow
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        y = y.reshape(1, y.size)
+        t = t.reshape(1, t.size)
+
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+
+    batch_size = y.shape[0]
+    delta = 1e-7
+
+    # yの各ベクトルについて、正解ラベルのインデックスにある要素を取り出す
+    biggests_in_y = y[np.arange(batch_size), t]
+
+    return -1 * np.sum(np.log(biggests_in_y + delta)) / batch_size
 
 def sigmoid(x):
     exp = math.exp(x)
---
+++ tosho/common/layers.py
@@ -0,0 +1,129 @@
+import numpy as np
+import sys, os
+sys.path.append(os.pardir)
+from common.functions import softmax, cross_entropy_error
+
+class AddLayer:
+    '''
+    加算を行うレイヤー
+    '''
+    def __init__(self):
+        pass
+    
+    def forward(self, x, y):
+        out = x + y
+
+        return out
+    
+    def backward(self, dout):
+        dx = dout * 1
+        dy = dout * 1
+
+        return dx, dy
+
+class MulLayer:
+    '''
+    積算を行うレイヤー
+    '''
+    def __init__(self):
+        self.x, self.y = None, None
+    
+    def forward(self, x, y):
+        self.x, self.y = x, y
+        out = x * y
+
+        return out
+    
+    def backward(self, dout):
+        dx = dout * self.y
+        dy = dout * self.x
+
+        return dx, dy
+
+class SigmoidLayer:
+    '''
+    Sigmoid関数を作用させるレイヤー
+    '''
+    def __init__(self):
+        self.out = None
+    
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+
+        return dx
+
+class AffineLayer:
+    '''
+     線形変換を行うレイヤー
+    '''
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None   # for calculate dW
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(self.W, x) + self.b
+
+        return out
+    
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        return dx
+
+class SoftmaxLayer:
+    '''
+    softmax変換を行うレイヤー
+    '''
+    def __init__(self):
+        self.y = None
+        self.o = None
+        self.loss = None
+    
+    def forward(self, x, y):
+        self.y = y
+        self.o = softmax(x)
+        self.loss = cross_entropy_error(self.o, self.y)
+
+        return self.loss
+
+    def backward(self, dout):
+        batch_size = self.y.shape[0]
+        if self.y.size == self.o.size:
+            dx = (self.o - self.y) / batch_size
+        else:
+            dx = self.o.copy()
+            dx[np.arange(batch_size), self.o] -= 1
+            dx = dx / batch_size
+        
+        return dx
+
+if __name__ == '__main__':
+    x, y = 5, 7
+
+    add = AddLayer()
+    out = add.forward(5, 7)
+    dx, dy = add.backward(1)
+    print(*['add:', x, y, out, dx, dy])
+
+    mul = MulLayer()
+    out = mul.forward(x, y)
+    dx, dy = mul.backward(1)
+    print(*['mul:', x, y, out, dx, dy])
+
+    sig = SigmoidLayer()
+    out = sig.forward([x, y])
+    dx = sig.backward([1, 1])
+    print(*['sigmoid:', x, y, out, dx])
+
