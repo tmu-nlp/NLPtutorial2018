6f3838a625238b94ca7c72a1faba7f8bfb5a4a2e
SP4-QAVION\qavio <qavion68210>
2018-07-09 15:53:05 +0900

    finish tutorial10

--- homma/tutorial10/cky.py
+++ homma/tutorial10/cky.py
@@ -1,4 +1,5 @@
 import math
+import nltk
 import os
 
 from itertools import product
@@ -11,7 +12,7 @@ def ijk_iter(words, nonterm):
         for i in range(j - 2, -1, -1): # スパンの左側（右から左へ）
             for k in range(i + 1, j):  # rsym の開始点
                 for sym, lsym, rsym, logprob in nonterm:
-                    yield f'{lsym};{i};{k}', f'{rsym};{k};{j}', f'{sym};{i};{j}', logprob
+                    yield f'{lsym} {i} {k}', f'{rsym} {k} {j}', f'{sym} {i} {j}', logprob
 
 
 def main():
@@ -24,15 +25,15 @@ def main():
 
     # "lhs \\t rhs \\t prob \\n" 形式の文法を読み込む
     nonterm = [] # （左，右１，右２，確率）の非終端記号
-    preterm = {} # pre[右] = [(左, 確率)...] 形式の辞書
+    preterm = defaultdict(list) # pre[右] = [(左, 確率)...] 形式の辞書
     for rule in open(grammar_path, encoding='utf8'):
         lhs, rhs, prob_str = rule.rstrip().split('\t')
-        prob = float(prob_str)
-        rhss = rhs.split(' ')
+        log_prob = math.log(float(prob_str))
+        rhss = rhs.split()
         if len(rhss) == 1:
-            preterm[rhss[0]] = (lhs, math.log(prob))
+            preterm[rhs].append((lhs, log_prob))
         else:
-            nonterm.append((lhs, rhss[0], rhss[1], math.log(prob)))
+            nonterm.append((lhs, rhss[0], rhss[1], log_prob))
 
     for line in open(input_path, encoding='utf8'):
         words = line.strip().split()
@@ -41,13 +42,12 @@ def main():
 
         # 前終端記号を追加
         for i in range(len(words)):
-            for lhs, log_prob in preterm.values():
-                # if preterm[words[i]][1] != 0:
-                best_score[f'{lhs};{i};{i+1}'] = log_prob
+            for lhs, log_prob in preterm[words[i]]:
+                best_score[f'{lhs} {i} {i+1}'] = log_prob
 
         # 非終端記号の組み合わせ
         for lsym_ik, rsym_kj, sym_ij, logprob in ijk_iter(words, nonterm):
-            if best_score[lsym_ik] > MINUS_INF and best_score[rsym_kj] > MINUS_INF:
+            if lsym_ik in best_score and rsym_kj in best_score:
                 my_lp = best_score[lsym_ik] + best_score[rsym_kj] + logprob
                 if my_lp > best_score[sym_ij]:
                     best_score[sym_ij] = my_lp
@@ -55,23 +55,17 @@ def main():
 
         def tree(sym_ij):
             '木をS式で返す'
-            sym, i_, j_ = sym_ij.split(';')
+            sym, i_, _ = sym_ij.split()
             i = int(i_)
             if sym_ij in best_edge:
-                best_edge_l = f'{best_edge[sym_ij][0]};{i_};{j_}'
-                best_edge_r = f'{best_edge[sym_ij][1]};{i_};{j_}'
-                return f'({sym} {best_edge_l} {best_edge_r})'
+                return f'({sym} {tree(best_edge[sym_ij][0])} {tree(best_edge[sym_ij][1])})'
             else: # 終端記号
                 return f'({sym} {words[i]})'
 
-        # print(best_edge)
-        print(tree(f'S;0;{len(words)-1}'))
-
-        # print(best_edge)
-        # print(best_score)
-
+        # 木を出力
+        print(tree(f'S 0 {len(words)}'))
 
 
 if __name__ == '__main__':
-    os.chdir(os.path.dirname(__file__))
+    # os.chdir(os.path.dirname(__file__))
     main()
